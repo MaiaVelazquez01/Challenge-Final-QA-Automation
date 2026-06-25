1c4577d10c7768fc5c248d8d93212b126efa7391
unknown <mvelazquezyarbe@gmail.com>
2026-06-25 13:19:16 -0300

    se suben hasta el cp15 inclusive

---
+++ cypress/e2e/myTestRestfulBooker.cy.js
@@ -0,0 +1,206 @@
+describe('Booking - Shady Meadows', () => {
+
+    beforeEach(() => {
+        cy.visit('https://automationintesting.online/')
+        cy.consultarDisponibilidad('15/07/2026', '16/07/2026')
+    })
+
+    it('CP1 - Reserva exitosa como usuario invitado', () => {
+        cy.intercept('POST', '**/api/booking').as('crearReserva')
+
+        cy.fixture('datosReserva').then((data) => {
+            const input = data.datosValidos
+            cy.completarFormularioReserva(input)
+            cy.reservarHabitacion('2026-07-15', '2026-07-16')
+            cy.validarReservaExitosa('@crearReserva')
+        })
+    })
+
+    it('CP2 - Reserva con campos vacíos', () => {
+        cy.intercept('POST', '**/api/booking').as('crearReservaConCamposVacios')
+
+        cy.get('.btn-primary').click()
+
+        cy.validarErrorReserva('@crearReservaConCamposVacios')
+
+        cy.verMsjsCamposObligatorios()
+        cy.verMsjErrorFN()
+        cy.verMsjErrorLN()
+        cy.verMsjErrorPhone()
+
+    })
+
+    it('CP3 - Reserva con dato FirstName de 2 caracteres', () => {
+        cy.intercept('POST', '**/api/booking').as('reservarConFNInvalido')
+
+        cy.fixture('datosReserva').then((data) => {
+            const input = data.datosConFNDe2Caracteres
+
+            cy.completarFormularioReserva(input)
+            cy.get('.btn-primary').click()
+
+            cy.verMsjErrorFN()
+        })
+
+        cy.validarErrorReserva('@reservarConFNInvalido')
+    })
+
+    it('CP4 - Reserva con dato FirstName de 3 caracteres', () => {
+        cy.intercept('POST', '**/api/booking').as('crearReservaConFNValido')
+
+        cy.fixture('datosReserva').then((data) => {
+            const input = data.datosConFNDe3Caracteres
+            cy.completarFormularioReserva(input)
+            cy.reservarHabitacion('2026-07-15', '2026-07-16')
+            cy.validarReservaExitosa('@crearReservaConFNValido')
+        })
+    })
+
+    it('CP5 - Reserva con dato FirstName de 18 caracteres', () => {
+        cy.intercept('POST', '**/api/booking').as('crearReservaConFNValido')
+
+        cy.fixture('datosReserva').then((data) => {
+            const input = data.datosConFNDe18Caracteres
+            cy.completarFormularioReserva(input)
+            cy.reservarHabitacion('2026-07-15', '2026-07-16')
+            cy.validarReservaExitosa('@crearReservaConFNValido')
+        })
+    })
+
+    it('CP6 - Reserva con dato FirstName de 19 caracteres', () => {
+        cy.intercept('POST', '**/api/booking').as('reservarConFNInvalido')
+
+        cy.fixture('datosReserva').then((data) => {
+            const input = data.datosConFNDe19Caracteres
+
+            cy.completarFormularioReserva(input)
+            cy.get('.btn-primary').click()
+
+            cy.verMsjErrorFN()
+        })
+
+        cy.validarErrorReserva('@reservarConFNInvalido')
+    })
+
+    it('CP7 - Reserva con dato LastName de 2 caracteres', () => {
+        cy.intercept('POST', '**/api/booking').as('reservarConLNInvalido')
+
+        cy.fixture('datosReserva').then((data) => {
+            const input = data.datosConLNDe2Caracteres
+
+            cy.completarFormularioReserva(input)
+            cy.get('.btn-primary').click()
+
+            cy.verMsjErrorLN()
+        })
+
+        cy.validarErrorReserva('@reservarConLNInvalido')
+    })
+
+    it('CP8 - Reserva con dato LastName de 3 caracteres', () => {
+        cy.intercept('POST', '**/api/booking').as('crearReservaConLNValido')
+
+        cy.fixture('datosReserva').then((data) => {
+            const input = data.datosConLNDe3Caracteres
+            cy.completarFormularioReserva(input)
+            cy.reservarHabitacion('2026-07-15', '2026-07-16')
+
+            cy.validarReservaExitosa('@crearReservaConLNValido')
+
+        })
+    })
+
+    it('CP9 - Reserva con dato LastName de 30 caracteres', () => {
+        cy.intercept('POST', '**/api/booking').as('crearReservaConLNValido')
+
+        cy.fixture('datosReserva').then((data) => {
+            const input = data.datosConLNDe30Caracteres
+            cy.completarFormularioReserva(input)
+            cy.reservarHabitacion('2026-07-15', '2026-07-16')
+            cy.validarReservaExitosa('@crearReservaConLNValido')
+        })
+    })
+
+    it('CP10 - Reserva con dato LastName de 31 caracteres', () => {
+        cy.intercept('POST', '**/api/booking').as('reservarConLNInvalido')
+
+        cy.fixture('datosReserva').then((data) => {
+            const input = data.datosConLNDe31Caracteres
+
+            cy.completarFormularioReserva(input)
+            cy.get('.btn-primary').click()
+
+            cy.verMsjErrorLN()
+        })
+
+        cy.validarErrorReserva('@reservarConLNInvalido')
+    })
+
+    it('CP11 - Reserva con dato Email inválido', () => {
+        cy.intercept('POST', '**/api/booking').as('reservarConEmailInvalido')
+
+        cy.fixture('datosReserva').then((data) => {
+            const input = data.datosConEmailInvalido
+
+            cy.completarFormularioReserva(input)
+            cy.get('.btn-primary').click()
+
+            cy.verMsjErrorEmail()
+        })
+
+        cy.validarErrorReserva('@reservarConEmailInvalido')
+    })
+
+    it('CP12 - Reserva con dato Phone de 10 caracteres', () => {
+        cy.intercept('POST', '**/api/booking').as('reservarConPhoneInvalido')
+
+        cy.fixture('datosReserva').then((data) => {
+            const input = data.datosConPhoneDe10Caracteres
+
+            cy.completarFormularioReserva(input)
+            cy.get('.btn-primary').click()
+
+            cy.verMsjErrorPhone()
+        })
+
+        cy.validarErrorReserva('@reservarConPhoneInvalido')
+    })
+
+    it('CP13 - Reserva con dato Phone de 11 caracteres', () => {
+        cy.intercept('POST', '**/api/booking').as('crearReservaConPhoneValido')
+
+        cy.fixture('datosReserva').then((data) => {
+            const input = data.datosConPhoneDe11Caracteres
+            cy.completarFormularioReserva(input)
+            cy.reservarHabitacion('2026-07-15', '2026-07-16')
+            cy.validarReservaExitosa('@crearReservaConPhoneValido')
+        })
+    })
+
+    it('CP14 - Reserva con dato Phone de 21 caracteres', () => {
+        cy.intercept('POST', '**/api/booking').as('crearReservaConPhoneValido')
+
+        cy.fixture('datosReserva').then((data) => {
+            const input = data.datosConPhoneDe21Caracteres
+            cy.completarFormularioReserva(input)
+            cy.reservarHabitacion('2026-07-15', '2026-07-16')
+            cy.validarReservaExitosa('@crearReservaConPhoneValido')
+        })
+    })
+
+    it('CP15 - Reserva con dato Phone de 22 caracteres', () => {
+        cy.intercept('POST', '**/api/booking').as('reservarConPhoneInvalido')
+        
+        cy.fixture('datosReserva').then((data) => {
+            const input = data.datosConPhoneDe22Caracteres
+
+            cy.completarFormularioReserva(input)
+            cy.get('.btn-primary').click()
+
+            cy.verMsjErrorPhone()
+        })
+
+        cy.validarErrorReserva('@reservarConPhoneInvalido')
+    })
+
+})
